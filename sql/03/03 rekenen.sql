614b01787c800e85cc722cfff0b82a5aea492deb
Fardin-Mohammed <39059@ma-web.nl>
2025-06-12 12:12:37 +0200

    1 van 03

---
+++ sql/03/03 rekenen.sql
@@ -0,0 +1,9 @@
+SELECT sum(leeftijd) from huisdieren
+
+SELECT AVG(leeftijd) from huisdieren
+
+SELECT AVG(leeftijd), SUM(leeftijd) from huisdieren
+
+SELECT SUM(leeftijd) from huisdieren
+
+SELECT SUM(leeftijd) as gemiddeldeLeeftijd from huisdieren
