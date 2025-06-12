487a1841d42af15b81e189dd2d73382c434ad942
Fardin-Mohammed <39059@ma-web.nl>
2025-06-12 12:23:38 +0200

    03 van 03 af

---
+++ sql/03/03 order.sql
@@ -0,0 +1,20 @@
+select * 
+from huisdieren 
+order by naam
+
+select * 
+from huisdieren 
+order by naam ASC
+
+select * 
+from huisdieren 
+order by naam DESC
+
+SELECT *
+FROM huisdieren
+ORDER BY typedier ASC, naam ASC;
+
+
+SELECT *
+FROM huisdieren
+ORDER BY typedier ASC, naam DESC;
