fe6741c3cb210dc3c2959a087e12b03aee4e7830
Fardin-Mohammed <39059@ma-web.nl>
2025-06-12 11:11:40 +0200

    01 van 02 is af

--- sql/02/01 huisdierdata.sql
+++ sql/02/01 huisdierdata.sql
@@ -1,1 +1,11 @@
-INSERT INTO `huisdieren` (`id`, `naam`, `eigenaar`, `typedier`, `leeftijd`, `geboortedatum`) VALUES (NULL, 'nyaa', 'animefan92', 'kat', '2', '2024-02-21');
+INSERT INTO `huisdieren` (`id`, `naam`, `eigenaar`, `typedier`, `leeftijd`, `geboortedatum`) 
+VALUES 
+(NULL, 'Flappie', 'paul', 'konijn', '4', '2024-02-01'),
+(NULL, 'nyaa', 'animefan92', 'kat', '2', '2024-02-21');
+
+INSERT INTO `huisdieren` (`id`, `naam`, `eigenaar`, `typedier`, `leeftijd`, `geboortedatum`) 
+VALUES 
+(NULL, 'Aap', 'stevan', 'gorilla', '6', '2024-02-11'),
+(NULL, 'gerard', 'aapfan7', 'ezel', '2', '2024-02-18'),
+(NULL, 'nick', 'sam', 'paard', '2', '2024-02-03'),
+(NULL, 'paul', 'autofan22', 'vogel', '1', '2024-02-27');
