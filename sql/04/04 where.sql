16fd306ad2bfcff50600fa97a47e6a8a3b2090c6
Fardin-Mohammed <39059@ma-web.nl>
2025-06-13 12:46:53 +0200

    01 van 04 is af

---
+++ sql/04/04 where.sql
@@ -0,0 +1,13 @@
+SELECT * FROM huisdieren where leeftijd <= 4
+
+SELECT * FROM huisdieren where leeftijd > 4
+
+SELECT * FROM huisdieren where leeftijd = 4
+
+SELECT  naam, 2
+FROM `huisdieren` 
+WHERE naam = "nick"
+
+SELECT  naam, 2
+FROM `huisdieren` 
+WHERE naam = "aap"
