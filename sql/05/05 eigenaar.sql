495cd425832b67e1b1e1a313058b462681afee39
Fardin-Mohammed <39059@ma-web.nl>
2025-06-16 14:39:11 +0200

    01 van 05 is af

---
+++ sql/05/05 eigenaar.sql
@@ -0,0 +1,15 @@
+CREATE TABLE huisdiereigenaar (
+    id INT,
+    voornaam VARCHAR(120),
+    achternaam VARCHAR(120),
+    leeftijd INT
+)
+
+INSERT INTO huisdiereigenaar (id, voornaam, achternaam, leeftijd) VALUES
+(1, 'Anna', 'Meijer', 14),
+(2, 'Stevan', 'Jansen', 17),
+(3, 'Aap', 'Fan', 13),
+(4, 'Sam', 'Bakker', 16),
+(5, 'Auto', 'Fan', 15),
+(6, 'Joep', 'Visser', 12),
+(7, 'Marije', 'Klaassen', 18)
