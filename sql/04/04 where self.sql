2657f9f923411260b723df2d9b82f683f16d0ef3
Fardin-Mohammed <39059@ma-web.nl>
2025-06-16 12:36:44 +0200

    04 van 04 is af

---
+++ sql/04/04 where self.sql
@@ -0,0 +1,17 @@
+SELECT * FROM `games` WHERE platform = "pc"
+
+SELECT * FROM `games` WHERE prijs <= 30
+
+SELECT * FROM `games` WHERE genre = "Puzzle" AND genre = "Action"
+
+SELECT * FROM `games` WHERE uitgebracht = 1
+
+SELECT * FROM `games` WHERE prijs = 29.99
+
+SELECT * 
+FROM `games` 
+WHERE prijs = 39.95 
+  AND genre = 'Cyberpunk' 
+  AND uitgebracht = 0
+
+  
